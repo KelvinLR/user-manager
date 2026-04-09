429aee1c4b0d4a97f8a9180ada5c4cd282bd4dc8
Kelvin <kelvindelima710@gmail.com>
2026-04-09 15:38:28 -0300

    feat: FPS and performance detection

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import { useEffect, useRef, useState } from "react"
 import { Stage, Layer, Rect } from "react-konva"
 import * as faceapi from "face-api.js"
 
+
 function App() {
   const videoRef = useRef<HTMLVideoElement | null>(null)
   const [faces, setFaces] = useState<any[]>([])
   const [videoReady, setVideoReady] = useState(false)
+
+  const [fps, setFps] = useState(0)
+  const [detectionTime, setDetectionTime] = useState(0)
+  const [totalTime, setTotalTime] = useState(0)
+
+  const fpsRef = useRef(0)
+  const lastTimeRef = useRef(performance.now())
+  const runningRef = useRef(false)
 
   // useEffects -> gerenciam coisas fora do contexto da UI
   // iniciar câmera
@@ -38,16 +47,26 @@ function App() {
         return
       }
 
+      const start = performance.now()
+
       try {
         const result = await faceapi.detectAllFaces(
           videoRef.current,
           new faceapi.TinyFaceDetectorOptions()
         )
 
+        const afterDetection = performance.now()
+
         const mapped = result.map(det => det.box)
         setFaces(mapped)
 
-        console.log("faces:", mapped)
+        requestAnimationFrame(() => {
+          const end = performance.now()
+
+          setDetectionTime(afterDetection - start)
+          setTotalTime(end - start)
+        })
+
       } catch (err) {
         console.error("erro detecção:", err)
       }
@@ -58,9 +77,30 @@ function App() {
     detect()
   }, [videoReady])
 
+  useEffect(() => {
+    if (runningRef.current) return
+    runningRef.current = true
+
+    function monitorFPS() {
+      const now = performance.now()
+      fpsRef.current++
+
+      if (now - lastTimeRef.current >= 1000) {
+        setFps(fpsRef.current)
+        fpsRef.current = 0
+        lastTimeRef.current = now
+      }
+
+      requestAnimationFrame(monitorFPS)
+    }
+
+    monitorFPS()
+  }, [])
+
   return (
-    <div style={{ position: "relative", width: 640, height: 480 }}>
-      
+    <body>
+      <h1>Webcam View</h1>
+      <div style={{ position: "relative", width: 640, height: 480, margin: "0 auto" }}>
       <video
         ref={videoRef}
         autoPlay
@@ -94,6 +134,10 @@ function App() {
         </Layer>
       </Stage>
     </div>
+    <p>FPS: {fps}</p>    <p>Detecção: {detectionTime.toFixed(1)} ms</p>
+    <p>Total: {totalTime.toFixed(1)} ms</p>
+    </body>
+    
   )
 }
 
